3e4f6c429362b1588804c9aa42039a21d6f926f6
cultleader <oddcultleader@gmail.com>
2020-07-26 13:09:27 -0500

    Kept both viz functions.
    Will end up deleting the old one most likely.

--- measures.py
+++ measures.py
@@ -241,3 +241,51 @@ def get_rdds_for_visuals_diff_graph(network, u, measure, radius, network2):
     return df     
 
   
+def realworld_distance_compare_for_visual(network, u, v, measure, radius, network2=None):
+    """Compares the radial distribution distance between two nodes in a single or two graphs.
+
+    Args
+    ----
+        network: a networkx Graph object
+        u: an instance of our Node class #  gets converted to string to match nx.Graph
+        v: an instance of our Node class #  gets converted to string to match nx.Graph
+        measure: a function that returns a list of values representing measures for each node
+        radius: the maximum radius we want to compare with
+        network2=None: Used if node v is from a different graph
+    """
+    # Get the shortest paths for each node up to the specified radius
+    real_paths1 = nx.single_source_shortest_path(network, u, radius)
+    if network2:
+        real_paths2 = nx.single_source_shortest_path(network2, v, radius)
+    else:
+        real_paths2 = nx.single_source_shortest_path(network, v, radius)
+    
+    # Create a list of Node objects from our shortest paths lists
+    node_list1 = populate_node_list(real_paths1)
+    node_list2 = populate_node_list(real_paths2)
+
+    measures_u = measure(network, node_list1)
+    if network2:
+        measures_v = measure(network2, node_list2)
+    else:
+        measures_v = measure(network, node_list2)
+
+    # take the list of degrees and set the appropriate field in all the Node objects in the list
+    add_measures_to_node(node_list1, measures_u)
+    add_measures_to_node(node_list2, measures_v)
+    
+    # for node in node_list1:
+    #     print(f'{node.name}, {node.measure}')
+    
+    # for node in node_list2:
+    #     print(f'{node.name}, {node.measure}')
+    
+
+    # gets the cumulative radial distributions for every radius up to threshhold
+    cRD1 = get_CRD(node_list1)
+    cRD2 = get_CRD(node_list2)
+    
+    # each radial distribution must go up to the same threshhold
+    ensure_radial_parity(cRD1, cRD2)
+
+    return get_rdd(cRD1, cRD2, node_list1, node_list2)
