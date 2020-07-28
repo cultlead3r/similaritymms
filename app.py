e4a25b9b7c4f1a7ebc230f6249a588fc2b8bd01b
cultleader <oddcultleader@gmail.com>
2020-07-28 00:14:50 -0500

    updated flaskapp and changed directory structure.

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ import visualize
 
 g1 = nx.Graph()
 
-
 g1.add_edges_from([
     (1, 2),
     (2, 3),
@@ -26,47 +25,53 @@ g1.add_edges_from([
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-       
-    html.H3("What measure do you want to use?"),
 
-    dcc.Dropdown(id='measure_choice', placeholder="Choose measure", options=[
-        {'label': 'triangles node is part of', 'value': 'triangles'},
-        {'label': 'global degree', 'value': 'global_graph_degree'},
-        {'label': 'local degree', 'value': 'local_graph_degree'},
-        {'label': 'global clique', 'value': 'global_graph_clique'},
-        {'label': 'local clique', 'value': 'local_graph_clique'},
-        
-    ]),
+    html.Div(className = 'row', children=[
+        html.Div(className='three columns div-user-controls', children=[
+            html.H3("What measure do you want to use?"),
 
-    dcc.Dropdown(id='network_choice', placeholder="Choose network", options=[
-    {'label': 'the sample graph', 'value': 'the_g1'}
-    ]),
+            dcc.Dropdown(id='measure_choice', placeholder="Choose measure", options=[
+                {'label': 'triangles node is part of', 'value': 'triangles'},
+                {'label': 'global degree', 'value': 'global_graph_degree'},
+                {'label': 'local degree', 'value': 'local_graph_degree'},
+                {'label': 'global clique', 'value': 'global_graph_clique'},
+                {'label': 'local clique', 'value': 'local_graph_clique'},
 
-    dcc.Graph(id='network'),
+            ]),
 
+            dcc.Dropdown(id='network_choice', placeholder="Choose network", options=[
+                {'label': 'the sample graph', 'value': 'the_g1'}
+            ]),
+]),
+            html.Div(className = 'nine columns div-for-charts bg-grey',
+                     children=[
+                         dcc.Graph(id='network'),
+                     ]),
+
+    ])
 ])
 
 
 @app.callback(
     Output('network', 'figure'),
     [
-     Input('network_choice', 'value'),
-     Input('measure_choice', 'value'),
+        Input('network_choice', 'value'),
+        Input('measure_choice', 'value'),
     ])
 def update_network(network_choice, measure_choice):
     network = None
     measure = None
     choice = None
     if measure_choice == 'triangles':
-        measure=measures.triangles
+        measure = measures.triangles
     elif measure_choice == 'global_graph_degree':
-        measure=measures.global_graph_degree
+        measure = measures.global_graph_degree
     elif measure_choice == 'local_graph_degree':
-        measure=measures.local_graph_degree
+        measure = measures.local_graph_degree
     elif measure_choice == 'global_graph_clique':
-        measure=measures.global_graph_clique
+        measure = measures.global_graph_clique
     elif measure_choice == 'local_graph_clique':
-        measure=measures.local_graph_clique
+        measure = measures.local_graph_clique
 
     if network_choice == 'the_g1':
         choice = g1
@@ -75,7 +80,7 @@ def update_network(network_choice, measure_choice):
         return fig
     else:
         fig = go.Figure()
-        fig.update_layout(template='plotly_dark')
+        fig.update_layout(template='plotly_dark', hovermode='x')
         return fig
 
 
